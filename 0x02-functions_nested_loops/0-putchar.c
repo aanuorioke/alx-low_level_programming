a35008688e492453f12a75f951ea72669a9454cf
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2023-03-21 10:17:15 -0700

    Added C program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints _putchar
+ * Return: success code
+ */
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}

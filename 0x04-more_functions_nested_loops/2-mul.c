691a99c96d0f40517cb1a1c1783ce38289f52d71
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2023-03-23 11:57:36 -0700

    Added programs to print numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - function
+ * Description: product of two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function
+ * Description: prints 0 - 14 ten times
+ * Return: Nothing
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 14; i++)
+	{
+		printf("%d", i);
+	}
+	_putchar('\n');
+}

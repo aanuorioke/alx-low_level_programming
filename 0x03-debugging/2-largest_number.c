816ec817ac32bb6addac11a1d1f867d38eb3a513
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2023-03-23 08:09:03 -0700

    Modified print larget

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -2,7 +2,6 @@
 
 /**
  * largest_number - returns the largest of 3 numbers
- * Description: returns the largest of 3 numbers
  * @a: first integer
  * @b: second integer
  * @c: third integer
@@ -13,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}

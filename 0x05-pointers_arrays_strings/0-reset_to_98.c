efc51deba189eceddd9e05a0c2f3c20cce8b0ee0
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2023-03-27 09:10:16 -0700

    Added program that updates value pointer points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  reset_to_98 - function
+ * Description: update value to 98
+ * @n:  pointer to the value to be updated
+ * Return: Nothing
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+learning about C pointers and arrays

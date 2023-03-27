e981e6190140f014ecd6da31f0b3ec451028825b
aanuorioke <oriokeaanuoluwapo@yahoo.com>
2023-03-27 11:00:48 -0700

    Modified print-array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,9 +9,12 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
+	if (n > 0)
+	{
+		int i;
 
-	for (i = 0; i < n - 1 ; i++) 
-		printf("%d, ", a[i]);
-	printf("%d\n", a[i]);
+		for (i = 0; i < n - 1 ; i++)
+			printf("%d, ", a[i]);
+		printf("%d\n", a[i]);
+	}
 }
